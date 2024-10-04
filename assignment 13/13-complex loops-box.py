9496a9d99d5547d5cf623334f16d8082aa47134b
unknown <hastishirazianorg@gmail.com>
2024-10-04 15:26:30 +0330

    pylearn

--- assignment 13/13-complex loops-box.py
+++ assignment 13/13-complex loops-box.py
@@ -5,17 +5,28 @@ class Pattern(arcade.Window):
     def __init__(self):
         super().__init__(width=500 , height=500 , title= "Complex loops-box")
         arcade.set_background_color(arcade.color.ALMOND)
-        self.line = 10
+        self.row = 10
+        self.column = 10
+        self.form_object = Form(self.design)
+
 
     def on_draw(self):
         arcade.start_render()
-
-        for line in range (10):
-            
+        self.form_object.draw()
 
 
+class Form(arcade.Sprite):
+
+    def __init__(self, Pattern):
+        for row in range (10):
+            for column in range(10):
+
+                if column%2 == 0:
+                    print("🔴")
+
+                elif column%2 == 1:
+                    print("🔵")
 
 
 object1 = Pattern()    
-
 arcade.run()

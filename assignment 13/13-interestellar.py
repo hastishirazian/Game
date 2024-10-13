c0e81f472b532b23293cfdb90dd8cbb124af0e2c
unknown <hastishirazianorg@gmail.com>
2024-10-14 00:43:48 +0330

    pylearn

--- assignment 13/13-interestellar.py
+++ assignment 13/13-interestellar.py
@@ -1,5 +1,7 @@
 import arcade
 import random
+from Spaceship import spaceship
+from Enemy import enemy
 
 class Game(arcade.Window):                                                      #arcade.Window is a class in arcade library
     
@@ -15,36 +17,22 @@ class Game(arcade.Window):                                                      
         arcade.draw_lrwh_rectangle_textured(0, 0, self.width, self.height, self.background)
         self.me.draw()
         self.enemy.draw()
+        arcade.finish_render()
 
     def on_key_press(self , symbol: int ,modifiers: int):
         
-        if symbol == 97:        # A
-            self.me.center_x = self.me.center_x - self.me.speed
-        elif symbol == 100:     # D
-            self.me.center_x = self.me.center_x + self.me.speed
+        if symbol == arcade.key.LEFT or symbol == arcade.key.A:      
+            self.me.move("L")
+        elif symbol == arcade.key.RIGHT or symbol == arcade.key.D:    
+            self.me.move("R")        
+        elif symbol == arcade.key.SPACE:
+            ...
 
     def on_update(self , delta_time: float):
-        self.enemy.center_y -= self.enemy.speed
+        self.enemy.move()
 
 
-    
-class spaceship(arcade.Sprite):
-    def __init__(self , game):                                                  #game in sapceship = self in game
-        super().__init__(":resources:images/space_shooter/playerShip1_orange.png")
-        self.center_x = game.width//2
-        self.center_y = 50
-        self.speed = 10
 
-
-  
-
-class enemy(arcade.Sprite):
-    def __init__(self , w , h):
-        super().__init__(":resources:images/space_shooter/playerShip1_blue.png")
-        self.center_x = random.randint(0,w)
-        self.center_y = h + 24
-        self.angle = 180
-        self.speed = 3
 
 
 

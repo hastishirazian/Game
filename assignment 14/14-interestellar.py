ac28463057df1ef1785f3762b34a811fd628e10e
unknown <hastishirazianorg@gmail.com>
2024-12-22 17:51:30 +0330

    pylearn

--- assignment 14/14-interestellar.py
+++ assignment 14/14-interestellar.py
@@ -10,31 +10,32 @@ class Game(arcade.Window):                                                      
         arcade.set_background_color(arcade.color.CATALINA_BLUE)
         self.background = arcade.load_texture(":resources:images/backgrounds/stars.png")
         self.me = spaceship(self)                   # object of spaceship
-        self.enemy = enemy(self.width, self.height) # object of enemy's spaceship
         self.enemys = []
 
     def on_draw(self):                                                          #This meyhod i for showing something
         arcade.start_render()
         arcade.draw_lrwh_rectangle_textured(0, 0, self.width, self.height, self.background)
         self.me.draw()
-        for enemy in self.enemys:
-            enemy.draw()
+        for i in self.enemys:
+            i.draw()
         arcade.finish_render()
 
     def on_key_press(self , symbol: int ,modifiers: int):
         
         if symbol == arcade.key.LEFT or symbol == arcade.key.A:      
-            self.me.move("L")
+            self.me.change_x = -1
         elif symbol == arcade.key.RIGHT or symbol == arcade.key.D:    
-            self.me.move("R")        
+            self.me.change_x = 1       
         elif symbol == arcade.key.SPACE:
             ...
 
     def on_update(self , delta_time: float):
-        for enemy in self.enemys:
-            self.enemy.move()
-        self.new_enemy = enemy(self.width, self.height)
-        self.enemys.append(self.new_enemy)
+        for i in self.enemys:
+            i.move()
+        if random.randint(1 , 100) == 5:
+            self.new_enemy = enemy(self.width, self.height)
+            self.enemys.append(self.new_enemy)
+
 
 
 
